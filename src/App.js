f24c6578b4c59720c9f33ac3b857268155b76af2
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-24 20:15:42 +0530

    material ui table added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   const [product, setProduct] = useState([]);
   const [search, setSearch] = useState("");
 
@@ -30,7 +65,7 @@ function App() {
           setSearch(e.target.value);
         }}
       />
-      {product
+      {/* {product
         .filter((item) => {
           if (search == "") {
             return item;
@@ -44,7 +79,25 @@ function App() {
               {item.name} -{item.price}
             </p>
           );
-        })}
+        })} */}
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Product Name</StyledTableCell>
+              <StyledTableCell align="right">Product Price</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <StyledTableRow>
+              <StyledTableCell component="th" scope="row">
+                Product Name
+              </StyledTableCell>
+              <StyledTableCell align="right">Product Price</StyledTableCell>
+            </StyledTableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
