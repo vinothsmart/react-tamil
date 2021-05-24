313a2db57a5080d8a4be173aba0295ce65d5842a
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-24 19:30:27 +0530

    fetching data using api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const getProductData = async () => {
+    try {
+      const data = await axios.get(
+        "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+      );
+      console.log(data);
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
