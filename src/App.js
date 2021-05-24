c1206a158e7c9307a8af8f985e12b2971e2f178a
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-24 19:36:44 +0530

    listing product name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [product, setProduct] = useState([]);
+
   const getProductData = async () => {
     try {
       const data = await axios.get(
         "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
       );
-      console.log(data);
+      // console.log(data.data);
+      setProduct(data.data);
     } catch (e) {}
   };
 
@@ -19,6 +22,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {product.map((item) => {
+        return <p>{item.name}</p>;
+      })}
     </div>
   );
 }
